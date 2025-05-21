9c3b91f8cfd2080cf49546662b78b11f8522e4f6
Md Ohidul(SM) <82235742+ohidulserniabat@users.noreply.github.com>
2025-05-21 17:21:20 +0600

    Add files via upload

---
+++ fake-intercom.js
@@ -0,0 +1,4 @@
+
+// Simulated attacker-controlled script
+alert("🔥 Supply Chain Attack Triggered! Your session is compromised.");
+document.body.innerHTML += "<p>Document.cookie: " + document.cookie + "</p>";
